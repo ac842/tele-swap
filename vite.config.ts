3d4dec44d4def74d9ed6a4e922b0922de11bd84d
ac842 <130188187+ac842@users.noreply.github.com>
2024-09-29 07:21:56 +0800

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import react from '@vitejs/plugin-react';
+import {defineConfig} from 'vite';
+import mkcert from 'vite-plugin-mkcert';
+import {nodePolyfills} from 'vite-plugin-node-polyfills';
+import Terminal from 'vite-plugin-terminal';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: './',
+    plugins: [
+        react(),
+        nodePolyfills({
+            globals: {
+                Buffer: true
+            }
+        }),
+        mkcert({force: true}),
+        Terminal()
+    ]
+});
