4ac1fa9fc0b78d811ac2ffd4400bc08c505fe41d
ac842 <130188187+ac842@users.noreply.github.com>
2024-09-30 21:42:04 +0800

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,28 @@ import { defineConfig } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import Terminal from 'vite-plugin-terminal';
+import fs from 'fs';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: './',
-    plugins: [
-        react(),
-        nodePolyfills({
-            globals: {
-                Buffer: true
-            }
-        }),
-        mkcert({ force: true }),
-        Terminal()
-    ],
-    server: {
-        port: 8088, // 修改端口為 8080
-        host: '0.0.0.0' // 允許外部訪問
-    }
+  base: './',
+  plugins: [
+    react(),
+    nodePolyfills({
+      globals: {
+        Buffer: true
+      }
+    }),
+    // 移除 mkcert 插件，因為我們已經有現成的證書
+    Terminal()
+  ],
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, '.key')), // 私鑰文件路徑
+      cert: fs.readFileSync(path.resolve(__dirname, '.pem')), // 證書文件路徑
+    },
+    port: 8088, 
+    host: '0.0.0.0' 
+  }
 });
