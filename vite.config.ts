ad3707aafbd449eca5d141c87b679493037c3312
ac842 <130188187+ac842@users.noreply.github.com>
2024-09-30 11:07:02 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
-import {nodePolyfills} from 'vite-plugin-node-polyfills';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import Terminal from 'vite-plugin-terminal';
 
 // https://vitejs.dev/config/
@@ -14,7 +14,11 @@ export default defineConfig({
                 Buffer: true
             }
         }),
-        mkcert({force: true}),
+        mkcert({ force: true }),
         Terminal()
-    ]
+    ],
+    server: {
+        port: 8088, // 修改端口為 8080
+        host: '0.0.0.0' // 允許外部訪問
+    }
 });
